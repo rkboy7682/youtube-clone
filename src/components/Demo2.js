209b93bf054ed7d1c5a3477ac7de7da59632476e
rkboy7682 <rkboy7682@gmail.com>
2024-01-29 01:33:22 +0530

    last session

--- src/components/Demo2.js
+++ src/components/Demo2.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import { current } from "@reduxjs/toolkit";
+import React, { useRef, useState } from "react";
 
 const Demo2 = () => {
   const [y, sety] = useState(0);
   let x = 10;
+  console.log("rendring");
+  const ref = useRef(0);
+  console.log(ref);
+
   return (
     <div className="w-96 h-96 border-2 border-black">
       <button
@@ -26,6 +31,17 @@ const Demo2 = () => {
         Click
       </button>
       <h1 className="font-bold text-xl m-2 p-2">Let Y:{y}</h1>
+
+      <button
+        className="border-2 bg-green-600 m-2 p-2 w-16"
+        onClick={() => {
+          ref.current = ref.current + 1;
+          console.log(ref.current);
+        }}
+      >
+        Click
+      </button>
+      <h1 className="font-bold text-xl m-2 p-2">Let ref:{ref.current}</h1>
     </div>
   );
 };
